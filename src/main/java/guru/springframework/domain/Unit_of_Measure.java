bb0a5e3d3203602a7410b1ab238ea370763b2c9f
yerasoni20 <yerasoni20@gmail.com>
2022-03-18 02:32:52 -0400

    Completed Assignment

---
+++ src/main/java/guru/springframework/domain/Unit_of_Measure.java
@@ -0,0 +1,30 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Unit_of_Measure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
